58b664d54c4405ac08d69dcec9f9c96dafd51459
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-07-03 16:02:19 +0100

    dynamic routing example

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 export default function Book() {
+  const { id } = useParams();
+  console.log("useParams id:", id);
   return (
     <div>
-      <h1>Book</h1>
+      <h1>Book {id}</h1>
     </div>
   );
 }
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function BookList() {
   return (
     <div>
       <h1>BookList</h1>
+      <ul>
+        <li>
+          <Link to="/books/1">Book 1</Link>
+        </li>
+        <li>
+          <Link to="/books/1">Book 2</Link>
+        </li>
+      </ul>
     </div>
   );
 }
